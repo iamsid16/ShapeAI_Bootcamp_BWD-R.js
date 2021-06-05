981161444137344b9f6a6827809b9709d25334a3
iamsid16 <85390205+iamsid16@users.noreply.github.com>
2021-06-05 14:59:04 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Web Development with Javascript and React.js</h1>
+      <p>
+        An awesome 7-days online bootcamp was conducted by Shaurya Sinha at
+        ShapeAI. Students got insights in Html,Javascript and React.js
+      </p>
+    </div>
+  );
+}
+
+export default Note;
